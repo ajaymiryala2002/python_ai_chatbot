061add9de5436e48f5f8cd10f3043f475d6b9d6e
ajaymiryala2002 <ajaymiryala259@gmail.com>
2026-01-16 10:26:07 +0530

    your message here

--- python_ai_agent/urls.py
+++ python_ai_agent/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1.views import chat_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",chat_view)
 ]
