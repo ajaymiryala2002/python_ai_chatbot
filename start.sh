daad07fc3f85eadae67d0c5f9ba7652e95f84598
ajaymiryala2002 <ajaymiryala259@gmail.com>
2026-01-16 11:18:13 +0530

    Add gunicorn and fix deployment scripts

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn AI.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn python_ai_agent.wsgi --bind 0.0.0.0:$PORT
